ad71bcfd66b79ec4b7a3eff65dd1df4bc129e4a9
Henrique Moraes <hrm8@duke.edu>
2013-10-27 21:40:51 -0400

    initial commit

--- src/eventBarrier/EventBarrier.java
+++ src/eventBarrier/EventBarrier.java
@@ -2,6 +2,41 @@ package eventBarrier;
 
 import api.AbstractEventBarrier;
 
-public class EventBarrier extends AbstractEventBarrier {
+public class EventBarrier extends AbstractEventBarrier implements Runnable{
+
+    @Override
+    public void arrive () {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void raise () {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void complete () {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int waiters () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void run () {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    public static void main(String args[]) {
+        EventBarrier barrier = new EventBarrier();
+        Thread thread1 = new Thread(barrier, "Test 1");
+    }
     
 }
