f5581880a3623fa654fec40c90948af45118a441
srwareham <srwareham@hotmail.com>
2013-11-04 00:08:56 -0500

    Set defalut to be EventBarrier run

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		if (args.length == 0) {
 			// No options specified; make the default as the part 3 elevator
 			// submission
+			EventExecutor ee = new EventExecutor();
 		} else if (args.length > 2) {
 			// Throw an error--too many args
 		} else // known just one arg
