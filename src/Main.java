7f7dd9a808d3d21624571350531acc955a495ad2
Henrique Moraes <hrm8@duke.edu>
2013-11-08 01:09:53 -0500

    Test main implementation

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,41 @@
+import java.util.Random;
 import simpleEventBarrier.SimpleEventExecutor;
+import elevator.Building;
+import elevator.Passenger;
 import eventBarrier.EventExecutor;
 
 public class Main {
 
-	public static void main(String args[]) {
-		if (args.length == 0) {
-			// No options specified; make the default as the part 3 elevator
-			// submission
-			SimpleEventExecutor ee = new SimpleEventExecutor();
-		} else if (args.length > 2) {
-			// Throw an error--too many args
-		} else // known just one arg
-		if (args[0].equals("p1")) {
-			// call the EventBarrier-- happens in the constructor.
-			EventExecutor ee = new EventExecutor();
-		}
-		/*
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            // No options specified; make the default as the part 3 elevator
+            // submission
+            Random generator = new Random(34569);
+            int numFloors = 100;
+            int id = 0;
+            Building b = new Building(numFloors, 1);
+            for (int i = 0; i < 1000; i++) {
+                int from = -1, to = -1;
+                while (from == to) {
+                    from = generator.nextInt(100);
+                    to = generator.nextInt(100);
+                }
+                
+                Passenger p = new Passenger(b, id,0,0 );
+                Thread t = new Thread(p,"Passenger "+id);
+                id++;
+                t.start();
+            }
+            
+            //SimpleEventExecutor ee = new SimpleEventExecutor();
+        } else if (args.length > 2) {
+            // Throw an error--too many args
+        } else // known just one arg
+            if (args[0].equals("p1")) {
+                // call the EventBarrier-- happens in the constructor.
+                EventExecutor ee = new EventExecutor();
+            }
+        /*
 		if (args[0].equals("p2part1")) {
 			// call the elevator part1
 		} else if (args[0].equals("p2part2")) {
@@ -23,6 +43,6 @@ public class Main {
 		} else if (args[0].equals("p2part3")) {
 			// call the elevator part3
 		}
-*/
+         */
 	}
 }
