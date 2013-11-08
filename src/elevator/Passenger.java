ef286c8f0c7e54fa9000a1fd05c9280ecd7354b8
Henrique Moraes <hrm8@duke.edu>
2013-11-08 00:01:48 -0500

    Make passenger wait for elevator to arrive

--- src/elevator/Passenger.java
+++ src/elevator/Passenger.java
@@ -34,23 +34,32 @@ public class Passenger implements Runnable {
 	}
 	
 	private synchronized void RideElevator(AbstractElevator elevator) {
-        
-            //Whats the difference between synchronizing the method or the elevator?
-                elevator.Enter();
-                //TODO Log passenger entering
-                elevator.RequestFloor(destinationFloor);
-                //TODO Log floor request
-                while (elevator.getCurrentFloor() != destinationFloor) {
-                    try {
-                        elevator.wait();
-                    } catch (InterruptedException e) {
-                        // TODO Write error on log
-                        e.printStackTrace();
-                    }
-                }
-                elevator.Exit();
-                //TODO Log passenger exiting
-            
-        }
+
+	    //Whats the difference between synchronizing the method or the elevator?
+	    while (elevator.getCurrentFloor() != fromFloor) {
+	        try {
+	            elevator.wait();
+	        }
+	        catch (InterruptedException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+	    }
+	    elevator.Enter();
+	    //TODO Log passenger entering
+	    elevator.RequestFloor(destinationFloor);
+	    //TODO Log floor request
+	    while (elevator.getCurrentFloor() != destinationFloor) {
+	        try {
+	            elevator.wait();
+	        } catch (InterruptedException e) {
+	            // TODO Write error on log
+	            e.printStackTrace();
+	        }
+	    }
+	    elevator.Exit();
+	    //TODO Log passenger exiting
+
+	}
 
 }
