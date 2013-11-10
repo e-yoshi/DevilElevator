bd8d7c4b010489b9bac1f81dc2d2d5b98cb673bf
eldermyoshida <elder.yoshida@gmail.com>
2013-11-10 16:13:25 -0500

    csv file

--- src/elevator/Joker.java
+++ src/elevator/Joker.java
@@ -2,6 +2,7 @@ package elevator;
 
 public class Joker extends Passenger {
 
+	
 	public Joker(Building b, int id, int from, int dest, int wrongFloor, boolean wait, boolean request) {
 		super(b, id, from, dest);
 		// TODO Auto-generated constructor stub
