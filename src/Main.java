b044760013069241ce67a10c1f4d679fc5439980
srwareham <srwareham@hotmail.com>
2013-11-10 16:05:26 -0500

    Starting Main refactoring

--- src/Main.java
+++ src/Main.java
@@ -15,23 +15,9 @@ public class Main {
         if (args.length == 0) {
             // No options specified; make the default as the part 3 elevator
             // submission
-            Random generator = new Random(34569);
-            int numFloors = 5;
-            int numPassengers = 5;
-            int id = 0;
-            Building b = new Building(numFloors, 2);
-            for (int i = 0; i < numPassengers; i++) {
-                int from = -1, to = -1;
-                while (from == to) {
-                    from = generator.nextInt(numFloors);
-                    to = generator.nextInt(numFloors);
-                }
-                
-                Passenger p = new Passenger(b, id,from,to);
-                Thread t = new Thread(p,"Passenger "+id);
-                id++;
-                t.start();
-            }
+//        	startP2Part3();
+        	startP1();
+   
             
             //SimpleEventExecutor ee = new SimpleEventExecutor();
         } else if (args.length > 2) {
@@ -39,16 +25,48 @@ public class Main {
         } else // known just one arg
             if (args[0].equals("p1")) {
                 // call the EventBarrier-- happens in the constructor.
-                EventExecutor ee = new EventExecutor();
+                startP1();
             }
-        /*
+        
 		if (args[0].equals("p2part1")) {
 			// call the elevator part1
+			startP2part1();
 		} else if (args[0].equals("p2part2")) {
+			startP2Part2();
 			// call the elevator part2
 		} else if (args[0].equals("p2part3")) {
+			startP2Part3();
 			// call the elevator part3
 		}
-         */
+         
 	}
+    
+    private static void startP1(){
+    	EventExecutor ee = new EventExecutor();
+    }
+    private static void startP2part1(){
+    	
+    }
+    private static void startP2Part2(){
+    	
+    }
+    private static void startP2Part3(){
+    	Random generator = new Random(34569);
+        int numFloors = 5;
+        int numPassengers = 5;
+        int id = 0;
+        Building b = new Building(numFloors, 2);
+        for (int i = 0; i < numPassengers; i++) {
+            int from = -1, to = -1;
+            while (from == to) {
+                from = generator.nextInt(numFloors);
+                to = generator.nextInt(numFloors);
+            }
+            
+            Passenger p = new Passenger(b, id,from,to);
+            Thread t = new Thread(p,"Passenger "+id);
+            id++;
+            t.start();
+        }
+    }
 }
