6f4bbfd638998e48f5c79f56c401213fa00ae97f
srwareham <srwareham@hotmail.com>
2013-11-10 19:48:13 -0500

    Added Jokers to ElevatorExecutor

--- src/elevator/ElevatorExecutor.java
+++ src/elevator/ElevatorExecutor.java
@@ -12,8 +12,9 @@ public class ElevatorExecutor {
 
 	private static int numFloors;
 	private static int numElevators;
+	private static int maxOccupancy = 15;
 
-	private Building building = null;
+	private static Building building = null;
 
 	public ElevatorExecutor(int partNumber) {
 		factory = new ElevatorFactory(FILENAME);
@@ -24,15 +25,16 @@ public class ElevatorExecutor {
 
 		if (partNumber == 1) {
 			numElevators = 1;
+			maxOccupancy = -1;
 			// TODO: need some sort of statement saying elevators will be
 			// unbounded
 		} else if (partNumber == 2) {
 			numElevators = 1;
-			// TODO: need some sort of statement saying elevators will be
-			// bounded
+			// This can be set to whatever value is desired for part 2
+			maxOccupancy = 12;
 		}
 
-		building = new Building(numFloors, numElevators);
+		building = new Building(numFloors, numElevators, maxOccupancy);
 
 		execute();
 	}
@@ -44,6 +46,12 @@ public class ElevatorExecutor {
 			Thread t = new Thread(p, "Passenger " + p.getID());
 			t.start();
 		}
+		
+//		for (Joker j: jokerList) {
+//			j.setBuilding(building);
+//			Thread t = new Thread(j, "Joker " + j.getID());
+//			t.start();
+//		}
 	}
 
 }
